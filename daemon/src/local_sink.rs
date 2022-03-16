975d9a804bbaccbe27030dc3897f01af09be8907
Evin Sellin <evinism@gmail.com>
2022-03-15 23:55:42 -0700

    what on earth did i do to this

--- daemon/src/local_sink.rs
+++ daemon/src/local_sink.rs
@@ -6,46 +6,103 @@ extern crate anyhow;
 extern crate clap;
 extern crate cpal;
 
+use std::sync::{Arc, Mutex};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use dasp::sample::Sample;
 
-pub fn local_sink(_audio_pipe: tokio::sync::mpsc::Receiver<Vec<(i16, i16)>>) -> anyhow::Result<()> {
-    let stream = stream_setup_for(sample_next)?;
+pub fn local_sink(mut audio_pipe: tokio::sync::mpsc::Receiver<Vec<(i16, i16)>>) -> anyhow::Result<()> {
+    let buffer = Vec::<f32>::new();
+    let buf_ref_1 = Arc::new(Mutex::new(buffer));
+    let buf_ref_2 = buf_ref_1.clone();
+
+    let (_host, device, config) = host_device_setup()?;
+
+    let sample_rate = config.sample_rate().0 as f32;
+    let nchannels = config.channels() as usize;
+    let mut request = SampleRequestOptions {
+        sample_rate,
+        nchannels,
+        sample_counter: 0
+    };
+    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
+
+    let stream = match config.sample_format() {
+        cpal::SampleFormat::F32 => device.build_output_stream(
+            &config.into(),
+            move |output: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                for frame in output.chunks_mut(request.nchannels) {
+                    let value: f32 = cpal::Sample::from::<f32>(&sampler(&mut request, &buf_ref_1));
+                    for sample in frame.iter_mut() {
+                        *sample = value;
+                    }
+                }
+            },
+            err_fn,
+        )?,
+        cpal::SampleFormat::I16 => device.build_output_stream(
+            &config.into(),
+            move |output: &mut [i16], _: &cpal::OutputCallbackInfo| {
+                for frame in output.chunks_mut(request.nchannels) {
+                    let value: i16 = cpal::Sample::from::<f32>(&sampler(&mut request, &buf_ref_1));
+                    for sample in frame.iter_mut() {
+                        *sample = value;
+                    }
+                }
+            },
+            err_fn,
+        )?,
+        cpal::SampleFormat::U16 => device.build_output_stream(
+            &config.into(),
+            move |output: &mut [u16], _: &cpal::OutputCallbackInfo| {
+                for frame in output.chunks_mut(request.nchannels) {
+                    let value: u16 = cpal::Sample::from::<f32>(&sampler(&mut request, &buf_ref_1));
+                    for sample in frame.iter_mut() {
+                        *sample = value;
+                    }
+                }
+            },
+            err_fn,
+        )?,
+    };
+
     stream.play()?;
-    std::thread::sleep(std::time::Duration::from_millis(3000));
+    tokio::spawn(async move {
+        loop {
+            let samples = audio_pipe.recv().await;
+            if let Ok(mut guard) = buf_ref_2.try_lock() {
+                for sample in samples.iter() {
+                    for msg in sample.iter() {
+                        let out = Sample::to_sample::<f32>(
+                            (msg.0 + msg.1) / 2
+                        );
+                        guard.push(Sample::from_sample(out));
+                    }
+                }
+            }
+        }
+    });
+    std::thread::sleep(std::time::Duration::from_millis(300000));
     Ok(())
 }
 
-fn sample_next(o: &mut SampleRequestOptions) -> f32 {
+fn sampler(o: &mut SampleRequestOptions, buf_ref: &Arc<Mutex<Vec<f32>>>) -> f32 {
     o.tick();
-    o.tone(440.) * 0.1 + o.tone(880.) * 0.1
-    // combination of several tones
+    if let Ok(guard) = buf_ref.try_lock() {
+        guard[o.sample_counter]
+    } else {
+        0.
+    }
 }
 
 pub struct SampleRequestOptions {
     pub sample_rate: f32,
-    pub sample_clock: f32,
+    pub sample_counter: usize,
     pub nchannels: usize,
 }
 
 impl SampleRequestOptions {
-    fn tone(&self, freq: f32) -> f32 {
-        (self.sample_clock * freq * 2.0 * std::f32::consts::PI / self.sample_rate).sin()
-    }
     fn tick(&mut self) {
-        self.sample_clock = (self.sample_clock + 1.0) % self.sample_rate;
-    }
-}
-
-pub fn stream_setup_for<F>(on_sample: F) -> Result<cpal::Stream, anyhow::Error>
-where
-    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static + Copy,
-{
-    let (_host, device, config) = host_device_setup()?;
-
-    match config.sample_format() {
-        cpal::SampleFormat::F32 => stream_make::<f32, _>(&device, &config.into(), on_sample),
-        cpal::SampleFormat::I16 => stream_make::<i16, _>(&device, &config.into(), on_sample),
-        cpal::SampleFormat::U16 => stream_make::<u16, _>(&device, &config.into(), on_sample),
+        self.sample_counter = self.sample_counter + 1;
     }
 }
 
@@ -62,47 +119,4 @@ pub fn host_device_setup(
     println!("Default output config : {:?}", config);
 
     Ok((host, device, config))
-}
-
-pub fn stream_make<T, F>(
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-    on_sample: F,
-) -> Result<cpal::Stream, anyhow::Error>
-where
-    T: cpal::Sample,
-    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static + Copy,
-{
-    let sample_rate = config.sample_rate.0 as f32;
-    let sample_clock = 0f32;
-    let nchannels = config.channels as usize;
-    let mut request = SampleRequestOptions {
-        sample_rate,
-        sample_clock,
-        nchannels,
-    };
-    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
-
-    let stream = device.build_output_stream(
-        config,
-        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
-            on_window(output, &mut request, on_sample)
-        },
-        err_fn,
-    )?;
-
-    Ok(stream)
-}
-
-fn on_window<T, F>(output: &mut [T], request: &mut SampleRequestOptions, mut on_sample: F)
-where
-    T: cpal::Sample,
-    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static,
-{
-    for frame in output.chunks_mut(request.nchannels) {
-        let value: T = cpal::Sample::from::<f32>(&on_sample(request));
-        for sample in frame.iter_mut() {
-            *sample = value;
-        }
-    }
 }
